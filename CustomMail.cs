90d57b04778e0947d459751027c404869b1ea9fd
Salim Serhat AKA <157348323+salimdev3469@users.noreply.github.com>
2025-01-29 17:14:18 +0300

    Update CustomMail.cs

--- CustomMail.cs
+++ CustomMail.cs
@@ -22,17 +22,17 @@ namespace OtomatikMail
             {
                 MailMessage ePosta = new MailMessage
                 {
-                    From = new MailAddress("salimaka2014@gmail.com"),
+                    From = new MailAddress("your-address@gmail.com"),
                     Subject = "Özel Mesaj",
                     Body = mailBody,
                     IsBodyHtml = false // Eğer HTML formatlı mesaj istersen true yapabilirsin
                 };
 
-                ePosta.To.Add("lestoltenberg2023@gmail.com");
+                ePosta.To.Add("target-address@gmail.com");
 
                 using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
                 {
-                    smtp.Credentials = new NetworkCredential("salimaka2014@gmail.com", "uipf ijlh wosw nrmo");
+                    smtp.Credentials = new NetworkCredential("your-address@gmail.com", "password");
                     smtp.EnableSsl = true;
                     smtp.Send(ePosta);
                 }
